edd984d72d695ffe0e674f89176f46429d879440
Jhonny Vargas <vargasj@ryanair.com>
2020-12-13 17:01:25 +0000

    Trying with bindings directly into project src.

--- build.rs
+++ build.rs
@@ -6,26 +6,26 @@ const BINDINGS_FILE: &str = "bindings.rs";
 const WRAPPER_FILE: &str = "wrapper.h";
 
 fn main() {
-    let output_path = PathBuf::from(var("OUT_DIR").expect("env variable OUT_DIR not found"));
-    let path_bindings_buf_src = output_path.join(BINDINGS_FILE);
-    let path_bindings_file_src = path_bindings_buf_src.as_os_str().to_str().unwrap();
+    // let output_path = PathBuf::from(var("OUT_DIR").expect("env variable OUT_DIR not found"));
+    // let path_bindings_buf_src = output_path.join(BINDINGS_FILE);
+    // let path_bindings_file_src = path_bindings_buf_src.as_os_str().to_str().unwrap();
 
     println!("cargo:rustc-link-search={}", "/usr/lib");
     println!("cargo:include={}", "/usr/include");
 
-    bindgen::Builder::default()
-        .header(WRAPPER_FILE)
-        .clang_args(&["-I", "/usr/include"])
-        .whitelist_function("aiImportFile")
-        .whitelist_type("aiPostProcessSteps")
-        .whitelist_type("aiPrimitiveType")
-        .whitelist_type("aiTextureType")
-        .whitelist_function("aiReleaseImport")
-        .whitelist_function("aiGetErrorString")
-        .generate()
-        .unwrap()
-        .write_to_file(path_bindings_file_src)
-        .unwrap();
+    // bindgen::Builder::default()
+    //     .header(WRAPPER_FILE)
+    //     .clang_args(&["-I", "/usr/include"])
+    //     .whitelist_function("aiImportFile")
+    //     .whitelist_type("aiPostProcessSteps")
+    //     .whitelist_type("aiPrimitiveType")
+    //     .whitelist_type("aiTextureType")
+    //     .whitelist_function("aiReleaseImport")
+    //     .whitelist_function("aiGetErrorString")
+    //     .generate()
+    //     .unwrap()
+    //     .write_to_file(path_bindings_file_src)
+    //     .unwrap();
 
     println!("cargo:rustc-flags=-l assimp");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-#![allow(improper_ctypes)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+mod bindings;
+pub use crate::bindings::*;
 
 use std::ffi::CStr;
 
