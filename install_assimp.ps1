57ee48f1fbe8eb5db6cccee22aab620c8f47a80e
Jhonny Vargas <vargasj@ryanair.com>
2020-12-15 15:01:22 +0000

    Trying a windows build.

--- install_assimp.ps1
+++ install_assimp.ps1
@@ -1,9 +1,9 @@
 $path_assimp_repo = "$GITHUB_WORKSPACE\assimp"
 $path_assimp_repo_build = "$GITHUB_WORKSPACE\assimp\build"
 $path_assimp_out = "$GITHUB_WORKSPACE\out"
-$llvm_path = "$($env:SystemDrive)\Program Files\LLVM\bin"
-$clang = "$llvm_path/clang.exe"
-$clangcpp = "$llvm_path/clang++.exe"
+#$llvm_path = "$($env:SystemDrive)\Program Files\LLVM\bin"
+#$clang = "$llvm_path/clang.exe"
+#$clangcpp = "$llvm_path/clang++.exe"
 
 git clone --depth 1 --branch v5.0.1 https://github.com/assimp/assimp.git ${path_assimp_repo}
 
@@ -11,6 +11,6 @@ mkdir $path_assimp_repo_build
 
 cd $path_assimp_repo_build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$clangcpp -DCMAKE_C_COMPILER=$clang -DCMAKE_INSTALL_PREFIX=path_assimp_out -G Ninja ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++.exe -DCMAKE_C_COMPILER=clang.exe -DCMAKE_INSTALL_PREFIX=path_assimp_out -G Ninja ..
 ninja
 ninja install
