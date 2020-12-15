0d5695bd67dfc1e43424a75db9caf80a9d384a51
Jhonny Vargas <vargasj@ryanair.com>
2020-12-15 15:58:45 +0000

    Trying a windows build.

--- install_assimp.ps1
+++ install_assimp.ps1
@@ -1,17 +1,22 @@
-$path_assimp_repo = "$GITHUB_WORKSPACE\assimp"
-$path_assimp_repo_build = "$GITHUB_WORKSPACE\assimp\build"
-$path_assimp_out = "$GITHUB_WORKSPACE\out"
+#$path_assimp_repo = "$GITHUB_WORKSPACE\assimp"
+#$path_assimp_repo_build = "$GITHUB_WORKSPACE\assimp\build"
+#$path_assimp_out = "$GITHUB_WORKSPACE\out"
 
 #$INCLUDE = "$($env:SystemDrive)\Program Files\LLVM\bin"
 #$OLDPATH = [System.Environment]::GetEnvironmentVariable('PATH','machine')
 #$NEWPATH = "$OLDPATH;$INCLUDE"
 #[Environment]::SetEnvironmentVariable("PATH", "$NEWPATH", "Machine")
 
-git clone --depth 1 --branch v5.0.1 https://github.com/assimp/assimp.git ${path_assimp_repo}
+#git clone --depth 1 --branch v5.0.1 https://github.com/assimp/assimp.git ${path_assimp_repo}
+#
+#mkdir $path_assimp_repo_build
+#cd $path_assimp_repo_build
+#
+#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=path_assimp_out -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang.exe" -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang++.exe" -DCMAKE_LINKER="C:/Program Files/LLVM/bin/lld-link.exe" -DCMAKE_RC_COMPILER="C:/Program Files/LLVM/bin/llvm-rc.exe" -G Ninja ..
+#ninja
+#ninja install
 
-mkdir $path_assimp_repo_build
-cd $path_assimp_repo_build
+git clone https://github.com/microsoft/vcpkg
+.\vcpkg\bootstrap-vcpkg.bat
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=path_assimp_out -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang.exe" -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang++.exe" -DCMAKE_LINKER="C:/Program Files/LLVM/bin/lld-link.exe" -DCMAKE_RC_COMPILER="C:/Program Files/LLVM/bin/llvm-rc.exe" -G Ninja ..
-ninja
-ninja install
+.\vcpkg\vcpkg install assimp
