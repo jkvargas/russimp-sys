e1758330e30ac49cdfc57aea876abe6a9d6d9a7e
Jhonny Vargas <vargasj@ryanair.com>
2020-12-15 14:42:41 +0000

    Trying a windows build.

--- install_assimp.ps1
+++ install_assimp.ps1
@@ -11,6 +11,6 @@ mkdir $path_assimp_repo_build
 
 cd $path_assimp_repo_build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=C:\Program Files\LLVM\bin\clang++.exe -DCMAKE_C_COMPILER=C:\Program Files\LLVM\bin\clang.exe -DCMAKE_INSTALL_PREFIX=path_assimp_out -G Ninja ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$clangcpp -DCMAKE_C_COMPILER=$clang -DCMAKE_INSTALL_PREFIX=path_assimp_out -G Ninja ..
 ninja
 ninja install
