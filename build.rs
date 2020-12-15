0b6ddffad8141358be234130c6e3e5d64d2bd632
Jhonny Vargas <vargasj@ryanair.com>
2020-12-15 13:37:16 +0000

    Trying a windows build.

--- build.rs
+++ build.rs
@@ -10,8 +10,13 @@ fn main() {
     // let path_bindings_buf_src = output_path.join(BINDINGS_FILE);
     // let path_bindings_file_src = path_bindings_buf_src.as_os_str().to_str().unwrap();
 
-    println!("cargo:rustc-link-search={}", "/usr/local/lib");
-    println!("cargo:include={}", "/usr/local/include");
+    if cfg!(windows) {
+        println!("cargo:rustc-link-search={}", assimp_path("lib").as_str());
+        println!("cargo:include={}", assimp_path("include").as_str());
+    } else {
+        println!("cargo:rustc-link-search={}", "/usr/local/lib");
+        println!("cargo:include={}", "/usr/local/include");
+    }
 
     // bindgen::Builder::default()
     //     .header(WRAPPER_FILE)
@@ -28,4 +33,13 @@ fn main() {
     //     .unwrap();
 
     println!("cargo:rustc-flags=-l assimp");
+}
+
+fn assimp_path(relative_path: &str) -> String {
+    let mut assimp_install_path = std::env::var("GITHUB_WORKSPACE").unwrap();
+
+    assimp_install_path.push_str("\\out\\");
+    assimp_install_path.push_str(relative_path);
+
+    assimp_install_path
 }
--- install_assimp.ps1
+++ install_assimp.ps1
@@ -1,6 +1,7 @@
 $path_assimp_repo = "$GITHUB_WORKSPACE\assimp"
 $path_assimp_repo_build = "$GITHUB_WORKSPACE\assimp\build"
 $path_assimp_out = "$GITHUB_WORKSPACE\out"
+$llvm_path = "$($env:SystemDrive)\Program Files\LLVM\bin"
 
 git clone --depth 1 --branch v5.0.1 https://github.com/assimp/assimp.git ${path_assimp_repo}
 
@@ -8,6 +9,6 @@ mkdir $path_assimp_repo_build
 
 cd $path_assimp_repo_build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_INSTALL_PREFIX=path_assimp_out -G Ninja ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="$llvm_path\clang++" -DCMAKE_C_COMPILER="$llvm_path\clang" -DCMAKE_INSTALL_PREFIX=path_assimp_out -G Ninja ..
 ninja
 ninja install
