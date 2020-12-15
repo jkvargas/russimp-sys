333d2c9c5b8ecec7849eed48cdb4eac5ded1ff3b
Jhonny Vargas <vargasj@ryanair.com>
2020-12-15 18:50:23 +0000

    Trying a windows build.

--- install_assimp.ps1
+++ install_assimp.ps1
@@ -1,22 +1,7 @@
-#$path_assimp_repo = "$GITHUB_WORKSPACE\assimp"
-#$path_assimp_repo_build = "$GITHUB_WORKSPACE\assimp\build"
-#$path_assimp_out = "$GITHUB_WORKSPACE\out"
-
-#$INCLUDE = "$($env:SystemDrive)\Program Files\LLVM\bin"
-#$OLDPATH = [System.Environment]::GetEnvironmentVariable('PATH','machine')
-#$NEWPATH = "$OLDPATH;$INCLUDE"
-#[Environment]::SetEnvironmentVariable("PATH", "$NEWPATH", "Machine")
-
-#git clone --depth 1 --branch v5.0.1 https://github.com/assimp/assimp.git ${path_assimp_repo}
-#
-#mkdir $path_assimp_repo_build
-#cd $path_assimp_repo_build
-#
-#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=path_assimp_out -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang.exe" -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang++.exe" -DCMAKE_LINKER="C:/Program Files/LLVM/bin/lld-link.exe" -DCMAKE_RC_COMPILER="C:/Program Files/LLVM/bin/llvm-rc.exe" -G Ninja ..
-#ninja
-#ninja install
-
 git clone https://github.com/microsoft/vcpkg
 .\vcpkg\bootstrap-vcpkg.bat
 
 .\vcpkg\vcpkg install assimp
+
+cd .\vcpkg
+Get-ChildItem -Recurse
