eab7444d272c0b65d311397d3ba59ac5233a237d
Jhonny Vargas <vargasj@ryanair.com>
2020-12-17 13:49:19 +0000

    Trying a windows build.

--- install_assimp.ps1
+++ install_assimp.ps1
@@ -2,8 +2,3 @@ git clone https://github.com/microsoft/vcpkg
 .\vcpkg\bootstrap-vcpkg.bat
 
 .\vcpkg\vcpkg install assimp
-
-#cp .\vcpkg\installed\x86-windows\libassimp-vc142-mt.lib .\vcpkg\installed\x86-windows\assimp.lib
-
-cd .\vcpkg
-Get-ChildItem -Recurse
