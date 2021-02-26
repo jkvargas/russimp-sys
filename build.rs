136623961d6a4ff07cd106583b8462655a9bddf6
Jhonny Knaak de Vargas <11624159+jkvargas@users.noreply.github.com>
2021-02-26 11:32:16 +0000

    generating bindings.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,24 @@
 
 use std::env::var;
 
+const BINDINGS_FILE: &str = "bindings.rs";
+const WRAPPER_FILE: &str = "wrapper.h";
+
 fn main() {
+    bindgen::Builder::default()
+        .header(WRAPPER_FILE)
+        .whitelist_type("aiPostProcessSteps")
+        .whitelist_type("aiPrimitiveType")
+        .whitelist_type("aiTextureType")
+        .whitelist_function("aiImportFile")
+        .whitelist_function("aiImportFileFromMemory")
+        .whitelist_function("aiReleaseImport")
+        .whitelist_function("aiGetErrorString")
+        .generate()
+        .unwrap()
+        .write_to_file(get_output_path(BINDINGS_FILE))
+        .unwrap();
+
     if cfg!(windows) {
         println!(
             "cargo:rustc-link-search={}",
@@ -18,6 +35,12 @@ fn main() {
         println!("cargo:include={}", "/usr/local/include");
         println!("cargo:rustc-flags=-l assimp");
     }
+}
+
+fn get_output_path<'a>(content: &str) -> String {
+    let output_path = PathBuf::from(var("OUT_DIR").expect("env variable OUT_DIR not found"));
+    let path_bindings_buf_src = output_path.join(content);
+    path_bindings_buf_src.as_os_str().to_str().unwrap().to_string()
 }
 
 fn assimp_path(relative_path: &str) -> String {
